a564159bd9bbf99418638df740219bdf12145f43
steveryb <steveryb@gmail.com>
2015-03-14 22:35:42 +0200

    Forgot to add this guy

---
+++ public/lib/filter_controls.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import ui from "github:components/jqueryui@1.11.4"
+
+export default React.createClass({
+  componentDidMount: function() {
+      var that = this;
+      $(function() {
+        that.props.keysToFilter.forEach(function(key) {
+            $( "#slider-range-"+key ).slider({
+              range: true,
+              min: that.props.display_ranges[key][0],
+              max: that.props.display_ranges[key][1],
+              step: 0.05,
+              values: [ that.props.display_ranges[key][0], that.props.display_ranges[key][1]],
+              slide: function( event, ui ) {
+                that.props.updateDisplayRange(key, ui.values);
+                $( "#"+key+"-values" ).val(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+              }
+            });
+            $( "#"+key+"-values").val(that.props.display_ranges[key][0] + " - "+ that.props.display_ranges[key][1]);
+        });
+      });
+  },
+
+  componentDidUpdate: function() {
+    var el = this.getDOMNode();
+  },
+
+  componentWillUnmount: function() {
+    var el = this.getDOMNode();
+  },
+
+  render: function() {
+    console.log(this.props);
+    return (
+      <div className="filters">
+      {this.props.keysToFilter.map(function(key){
+        return (
+        <div key={key}>
+        <p>
+          <label for={key+"-values"}>{key} range:</label>
+          <input type="text" id={key+"-values"}></input>
+        </p>
+        <div id={"slider-range-"+key}></div> 
+        </div>)
+        })}
+      </div>
+    );
+  }
+});
