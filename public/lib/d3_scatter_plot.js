156467bedf57b1d372965ce443ebcd150bbd008a
steveryb <steveryb@gmail.com>
2015-03-14 15:23:17 +0200

    Use symbols rather than circles
    Instead of using a circle, we now use symbols based on the plate number

--- public/lib/d3_scatter_plot.js
+++ public/lib/d3_scatter_plot.js
@@ -8,8 +8,8 @@ var d3Chart = {};
 var tooltipVisibleOpacity = 0.9,
     tooltipHiddenOpacity = 0,
     circleSelectedOpacity = 1.0,
-    circleSelectedRadius = 10,
-    circleNormalRadius = 3.5,
+    pointSelectedSize = 200,
+    pointNormalSize = 50,
     circleNormalOpacity = 0.6,
     xAxisYOffset = 10,
     yAxisYOffset = 10,
@@ -68,6 +68,7 @@ d3Chart.create = function(el, props, state) {
 
 d3Chart.update = function(el, state) {
     var that = this;
+    // Axes and scales
     var x = d3.scale.linear()
         .range([0, this.props.width]);
 
@@ -87,109 +88,154 @@ d3Chart.update = function(el, state) {
     y.domain(d3.extent(state.data, function(d) { return d.lumo; })).nice();
   d3.select("#scatterplot-x-axis").call(xAxis)
   d3.select("#scatterplot-y-axis").call(yAxis)
+
+  // Points
   this.props.svg.selectAll(".dot")
       .data(state.data)
-    .enter().append("circle")
+    .enter().append("path")
       .attr("class","dot")
-      .attr("r", circleNormalRadius)
-      .attr("cx", function(d) {
-          return x(d.fic);
+      //.attr("r", circleNormalRadius)
+      .attr("transform", function(d) {
+          var pos = that._calcPosition(d, x, y);
+          return "translate(" + pos[0] + "," + pos[1] + ")";
       })
-      .attr("cy", function(d) {
-          return y(d.lumo);
-      })
+      .attr("d", d3.svg.symbol()
+                      .size(function(d) { return pointNormalSize; })
+                      .type(function(d) { return that._getSVGSymbol(d.plate_num)}))
       .on("mouseover", function(d) {
-      var circ = d3.select(this);
-      var matrix = this.getScreenCTM()
-                .translate(+this.getAttribute("cx"),
-                +this.getAttribute("cy"));
-      var xMatrix = this.getScreenCTM()
-                .translate(0,that.props.height);
-      var yMatrix = this.getScreenCTM()
-                .translate(0,0);
-         var left = matrix.e + window.pageXOffset
-         var top =  matrix.f + window.pageYOffset
-          d3.select(".tooltip").transition()
-               .duration(tooltipFadeinTime)
-               .style("opacity", tooltipVisibleOpacity);
-          d3.select(".x-tooltip").transition()
-               .duration(tooltipFadeinTime)
-               .style("opacity", tooltipVisibleOpacity);
-          d3.select(".y-tooltip").transition()
-               .duration(tooltipFadeinTime)
-               .style("opacity", tooltipVisibleOpacity);
-          d3.select(".tooltip").html(
-                "Drug A: " + that._formatNumber(d.a) + "<br/>" +
-                "Drug B: " + that._formatNumber(d.b) + "<br/>" +
-                "Drug C: " + that._formatNumber(d.c) + "<br/>")
-               .style("left", left + 10 + "px")
-               .style("top", top - 30+ "px");
-          d3.select(".x-tooltip").html(that._formatNumber(d.fic))
-               .style("left", left - 15 + "px")
-               .style("top", (window.pageYOffset + xMatrix.f) + "px");
-          d3.select(".y-tooltip").html(that._formatNumber(d.lumo))
-               .style("left", (window.pageXOffset + yMatrix.e - 50) + "px")
-               .style("top", top - 10 + "px");
-          that.props.svg.append("svg:line")
-              .attr("class","x-drop-line")
-              .attr("x1", d3.select(this).attr("cx"))
-              .attr("x2", d3.select(this).attr("cx"))
-              .attr("y1", d3.select(this).attr("cy"))
-              .attr("y2", that.props.height)
-              .attr("stroke",d3.select(this).style("fill"))
-              .attr("stroke-width","2");
-          that.props.svg.append("svg:line")
-              .attr("class","y-drop-line")
-              .attr("x1", 0)
-              .attr("x2", d3.select(this).attr("cx"))
-              .attr("y1", d3.select(this).attr("cy"))
-              .attr("y2", d3.select(this).attr("cy"))
-              .attr("stroke",d3.select(this).style("fill"))
-              .attr("stroke-width","2");
-         circ.transition()
-            .duration(tooltipFadeinTime)
-            .style("fill-opacity", circleSelectedOpacity)
-            .attr("r", circleSelectedRadius);
+        that._displayTooltip(this, d, that._calcPosition(d, x, y));
       })
       .on("mouseout", function(d) {
-          d3.selectAll(".x-drop-line").remove();
-          d3.selectAll(".y-drop-line").remove();
-          d3.select(".tooltip").transition()
-               .duration(500)
-               .style("opacity", 0);
-          d3.select(".x-tooltip").transition()
-               .duration(500)
-               .style("opacity", 0);
-          d3.select(".y-tooltip").transition()
-               .duration(500)
-               .style("opacity", 0);
-         d3.select(this).transition()
-            .duration(500)
-            .style("fill-opacity", circleNormalOpacity)
-            .attr("r", 3.5);
+        that._removeTooltip(this, d);
       })
       .style("fill-opacity", circleNormalOpacity)
       .style("fill", function(d) { return color(d.plate_num); });
 
+  // Legend
   var legend = this.props.svg.selectAll(".legend")
       .data(color.domain())
     .enter().append("g")
       .attr("class","legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      .attr("transform", function(d, i) { return "translate(" + that.props.width + "," + i * 20 + ")"; });
 
-  legend.append("rect")
-      .attr("x", this.props.width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
+  legend.append("path")
+      .attr("transform", function(d) {
+          var pos = that._calcPosition(d, x, y);
+          return "translate(" + -14 + "," + 7 + ")";
+      })
+      .attr("d", d3.svg.symbol()
+          .size(function(d) { return pointNormalSize; })
+          .type(function(d, i) { return that._getSVGSymbol(i)}))
       .style("fill", color);
 
   legend.append("text")
       .attr("class","legend-text")
-      .attr("x", this.props.width - 24)
+      .attr("x", -24)
       .attr("y", 9)
       .attr("dy", ".35em")
-      .text(function(d) { return "Plate number " + d; });
+      .text(function(d) { return "Plate number " + d + ": "; });
 };
+
+d3Chart._calcPosition = function(d, x, y) {
+    return [x(d.fic), y(d.lumo)];
+};
+
+d3Chart._removeTooltip = function(elm, d) {
+  // Hide tooltips
+  this._hideTooltip(d3.select(".tooltip"));
+  this._hideTooltip(d3.select(".x-tooltip"));
+  this._hideTooltip(d3.select(".y-tooltip"));
+
+  // Remove droplines
+  d3.selectAll(".x-drop-line").remove();
+  d3.selectAll(".y-drop-line").remove();
+
+  // unhighlight point
+  d3.select(elm).transition()
+    .duration(500)
+    .style("fill-opacity", circleNormalOpacity)
+    .attr("d", d3.svg.symbol()
+            .type(this._getSVGSymbol(d.plate_num))
+            .size(pointNormalSize))
+}
+
+d3Chart._hideTooltip = function(sel) {
+  sel.transition()
+       .duration(500)
+       .style("opacity", 0);
+
+}
+
+d3Chart._fadeInTooltip = function(sel) {
+  sel.transition()
+       .duration(tooltipFadeinTime)
+       .style("opacity", tooltipVisibleOpacity);
+}
+
+d3Chart._formatDrugCon = function(d) {
+    return "Drug A: " + this._formatNumber(d.a) + "<br/>" +
+    "Drug B: " + this._formatNumber(d.b) + "<br/>" +
+    "Drug C: " + this._formatNumber(d.c) + "<br/>"
+}
+
+d3Chart._getSVGSymbol = function(plate_num) {
+    return d3.svg.symbolTypes[plate_num];
+}
+
+d3Chart._displayTooltip = function(elm, d, pos) {
+    // Position calculations
+    var point = d3.select(elm);
+    var matrix = elm.getScreenCTM()
+                //.translate(pos[0], pos[1]);
+                .translate(0, 0);
+    var xMatrix = elm.getScreenCTM()
+                .translate(0, -pos[1] + this.props.height);
+    var yMatrix = elm.getScreenCTM()
+                .translate(-pos[0],0);
+    console.log(matrix, xMatrix, yMatrix);
+    var left = matrix.e + window.pageXOffset;
+    var top =  matrix.f + window.pageYOffset;
+
+     // Tooltips
+    this._fadeInTooltip(d3.select(".tooltip"));
+    this._fadeInTooltip(d3.select(".x-tooltip"));
+    this._fadeInTooltip(d3.select(".y-tooltip"));
+    d3.select(".tooltip").html(this._formatDrugCon(d))
+           .style("left", left + 10 + "px")
+           .style("top", top - 30+ "px");
+    d3.select(".x-tooltip").html(this._formatNumber(d.fic))
+           .style("left", left - 15 + "px")
+           .style("top", (window.pageYOffset + xMatrix.f) + "px");
+    d3.select(".y-tooltip").html(this._formatNumber(d.lumo))
+           .style("left", (window.pageXOffset + yMatrix.e - 50) + "px")
+           .style("top", top - 10 + "px");
+
+      // Drop Lines
+    this.props.svg.append("svg:line")
+          .attr("class","x-drop-line")
+          .attr("x1", pos[0])
+          .attr("x2", pos[0])
+          .attr("y1", pos[1])
+          .attr("y2", this.props.height)
+          .attr("stroke",point.style("fill"))
+          .attr("stroke-width","2");
+    this.props.svg.append("svg:line")
+          .attr("class","y-drop-line")
+          .attr("x1", 0)
+          .attr("x2", pos[0])
+          .attr("y1", pos[1])
+          .attr("y2", pos[1])
+          .attr("stroke", point.style("fill"))
+          .attr("stroke-width","2");
+
+     // Highlight point
+     point.transition()
+        .duration(tooltipFadeinTime)
+        .style("fill-opacity", circleSelectedOpacity)
+        .attr("d", d3.svg.symbol()
+                .type(this._getSVGSymbol(d.plate_num))
+                .size(pointSelectedSize))
+}
 
 d3Chart.destroy = function(el) {
 };
